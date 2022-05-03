ec25587c97977ccf327a85b15382e3b45ed8c856
Omer Karakas <karakasomer@gmail.com>
2022-05-04 00:26:48 +0300

    a few minor changes

--- Light Dark Mode/script.js
+++ Light Dark Mode/script.js
@@ -21,10 +21,33 @@ function switchTheme(event) {
     lightMode();
 
   document.documentElement.setAttribute("data-theme", theme);
+  localStorage.setItem("theme", theme);
 }
 
 function darkMode() {
   nav.style.backgroundColor = "rgb(0 0 0 /50%)";
   textBox.style.backgroundColor = "rgb(255 255 255 / 50%";
-  console.log(toggleIcon.children);
+  toggleIcon.children[0].textContent = "Dark Mode";
+  toggleIcon.children[1].classList.replace("fa-sun", "fa-moon");
+  image1.src = "img/undraw_proud_coder_dark.svg";
+  image2.src = "img/undraw_feeling_proud_dark.svg";
+  image3.src = "img/undraw_conceptual_idea_dark.svg";
+}
+
+function lightMode() {
+  nav.style.backgroundColor = "rgb(255 255 255 /50%)";
+  textBox.style.backgroundColor = "rgb(0 0 0 / 50%";
+  toggleIcon.children[0].textContent = "Light Mode";
+  toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
+  image1.src = "img/undraw_proud_coder_light.svg";
+  image2.src = "img/undraw_feeling_proud_light.svg";
+  image3.src = "img/undraw_conceptual_idea_light.svg";
+}
+
+const currentTheme = localStorage.getItem("theme") ?? "light";
+
+document.documentElement.setAttribute("data-theme", currentTheme);
+if (currentTheme === "dark") {
+  toggleSwitch.checked = true;
+  darkMode();
 }
